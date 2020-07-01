546edff86e2801bd610b9d384b13c167240dcd4e
v-v-d <v-v-d@bk.ru>
2020-07-02 00:08:02 +0300

    service send email bugfix

--- todo_api/todo/services.py
+++ todo_api/todo/services.py
@@ -5,7 +5,11 @@ from todo_api import settings
 
 def send_reminder_email(email, todo_item):
     title = f'You have an event scheduled'
-    message = f'Name: {todo_item.name}\nText: {todo_item.text}\nDate: {todo_item.date}'
+    name = todo_item.get("name")
+    text = todo_item.get("text")
+    date = todo_item.get("date")
+
+    message = f'Name: {name}\nText: {text}\nDate: {date}'
 
     return send_mail(
         title, message, settings.EMAIL_HOST_USER, [email], fail_silently=False
