aea3209d739d1c16f5a0b99cbbd31d3b35d1e43b
v-v-d <v-v-d@bk.ru>
2020-07-01 22:56:20 +0300

    signal serialize bugfix

--- todo_api/todo/signals.py
+++ todo_api/todo/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.forms.models import model_to_dict
 from django.utils import timezone
 
 from todo.models import Todo
@@ -11,7 +12,8 @@ def start_send_reminder_email_task(sender, instance, **kwargs):
     todo_date = instance.date
     reminder_date = todo_date - timezone.timedelta(minutes=1)
     user_email = instance.user.email
+    todo_item = model_to_dict(instance)
 
     send_reminder_email_task.apply_async(
-        email=user_email, todo=instance, eta=reminder_date
+        email=user_email, todo=todo_item, eta=reminder_date
     )
