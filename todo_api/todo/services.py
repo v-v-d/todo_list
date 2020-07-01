04e0291727d13a97f67c22c1e990f46024f63acb
v-v-d <v-v-d@bk.ru>
2020-07-02 00:20:38 +0300

    service datetime bugfix

--- todo_api/todo/services.py
+++ todo_api/todo/services.py
@@ -1,13 +1,15 @@
+from datetime import datetime
+
 from django.core.mail import send_mail
 
 from todo_api import settings
 
 
 def send_reminder_email(email, todo_item):
-    title = f'You have an event scheduled'
+    title = 'Your event will be soon.'
     name = todo_item.get("name")
     text = todo_item.get("text")
-    date = todo_item.get("date")
+    date = datetime.strptime(todo_item.get("date"), "%Y-%m-%dT%H:%M:%SZ")
 
     message = f'Name: {name}\nText: {text}\nDate: {date}'
 
